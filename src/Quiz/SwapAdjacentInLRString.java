8e0e9e74a0cc7c4d3f31ad0f2458fd42e96cfcd4
zhang <zhangwei@team-lab.com>
2019-05-23 10:04:04 +0900

    update

--- src/Quiz/SwapAdjacentInLRString.java
+++ src/Quiz/SwapAdjacentInLRString.java
@@ -1,4 +1,37 @@
 package Quiz;
 
 public class SwapAdjacentInLRString {
+
+    public boolean canTransform(String start, String end) {
+        int i = 0;
+        int j = 0;
+        int len = start.length();
+        while (i < len && j < len) {
+            while (i < len && start.charAt(i) == 'X') {
+                i++;
+            }
+            while (j < len && end.charAt(j) == 'X') {
+                j++;
+            }
+            if (i == len && j == len) {
+                return true;
+            }
+            if (i == len && j != len) {
+                return false;
+            }
+            if (i != len && j == len) {
+                return false;
+            }
+            if (start.charAt(i) != end.charAt(j)) {
+                return false;
+            }
+            if (start.charAt(i) == 'R' && i > j) {
+                return false;
+            }
+            if (start.charAt(i) == 'L' && i < j) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
