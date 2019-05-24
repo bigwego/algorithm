c5431383ce735e14a74853a21babd8e5aa596a16
zhang <zhangwei@team-lab.com>
2019-05-24 09:15:56 +0900

    update

---
+++ src/BinaryTree/MaximumBinaryTreeII.java
@@ -0,0 +1,14 @@
+package BinaryTree;
+
+public class MaximumBinaryTreeII {
+
+    private TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root != null && root.val > val) {
+            root.right = insertIntoMaxTree(root.right, val);
+            return root;
+        }
+        TreeNode node = new TreeNode(val);
+        node.left = root;
+        return node;
+    }
+}
