0206a5bb7f55e246987e80a111584296d1af3ced
zhang <zhangwei@team-lab.com>
2019-05-25 18:09:37 +0900

    update

--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,16 @@
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main() {
+        System.out.println(a());
+    }
 
+    private static String a() {
+        String str = "return";
+        try {
+            return str;
+        } finally {
+            String a = "a";
+            return a;
+        }
     }
 }
