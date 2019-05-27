2129fa2acaf7b6a925c19dc254a3e3d375ccdded
zhang <zhangwei@team-lab.com>
2019-05-28 08:01:34 +0900

    update

---
+++ src/Array/MaximumSumOf2NonOverlappingSubarrays.java
@@ -0,0 +1,4 @@
+package Array;
+
+public class MaximumSumOf2NonOverlappingSubarrays {
+}
---
+++ src/Others/ConvertBaseNeg2.java
@@ -0,0 +1,13 @@
+package Others;
+
+public class ConvertBaseNeg2 {
+
+    public String baseNeg2(int N) {
+        StringBuilder sb = new StringBuilder();
+        while (N != 0) {
+            sb.append(N & 1);
+            N = -(N >> 1);
+        }
+        return sb.length() != 0 ? sb.reverse().toString() : "0";
+    }
+}
