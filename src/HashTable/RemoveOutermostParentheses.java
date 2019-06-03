5f2081f1b0319ce5875e3d3e882ce1e7ce511815
zhang <zhangwei@team-lab.com>
2019-06-04 08:22:10 +0900

    update

--- src/HashTable/RemoveOutermostParentheses.java
+++ src/HashTable/RemoveOutermostParentheses.java
@@ -19,4 +19,18 @@ public class RemoveOutermostParentheses {
         }
         return sb.toString();
     }
+
+    public String removeOuterParentheses2(String S) {
+        StringBuilder sb = new StringBuilder();
+        int s = 0;
+        int cnt = 1;
+        for (int i = 1, len = S.length(); i < len; i++) {
+            cnt += S.charAt(i) == '(' ? 1 : -1;
+            if (cnt == 0) {
+                sb.append(S.substring(s, i));
+                s = i + 1;
+            }
+        }
+        return sb.toString();
+    }
 }
