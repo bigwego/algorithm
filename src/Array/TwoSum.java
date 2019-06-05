e03cebe6873b9f3d91ba306b22903cbed64a71c3
zhang <zhangwei@team-lab.com>
2019-06-06 07:28:02 +0900

    update;

--- src/Array/TwoSum.java
+++ src/Array/TwoSum.java
@@ -16,4 +16,15 @@ public class TwoSum {
         }
         return null;
     }
+
+    public int[] twoSum2(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0, len = numbers.length; i < len; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                return new int[]{ map.get(target - numbers[i]), i };
+            }
+            map.put(numbers[i], i);
+        }
+        return null;
+    }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -2,21 +2,4 @@ import BinaryTree.TreeNode;
 
 public class Solution {
 
-    int sum = 0;
-
-    public TreeNode bstToGst(TreeNode root) {
-        if (root == null || (root.left == null && root.right == null)) {
-            return null;
-        }
-        if (root.right != null) {
-            bstToGst(root.right);
-        }
-        root.val += sum;
-        sum += root.val;
-        if (root.left != null) {
-            bstToGst(root.left);
-        }
-        return root;
-    }
-
 }
