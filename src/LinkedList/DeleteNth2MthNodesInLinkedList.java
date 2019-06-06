847e84805df2622ca7a395ae8abd9d7f78b3708e
zhang <zhangwei@team-lab.com>
2019-06-07 08:32:02 +0900

    udpate

--- src/LinkedList/DeleteNth2MthNodesInLinkedList.java
+++ src/LinkedList/DeleteNth2MthNodesInLinkedList.java
@@ -1,4 +1,30 @@
 package LinkedList;
 
 public class DeleteNth2MthNodesInLinkedList {
+
+    public ListNode deleteNode(ListNode head, int n, int m) {
+        int idx = 0;
+        ListNode from = null;
+
+        while (idx++ < n) {
+            if (from == null) {
+                from = head;
+            } else {
+                from = from.next;
+            }
+        }
+
+        ListNode to = head;
+        idx = 0;
+        while (idx++ <= m) {
+            to = to.next;
+        }
+
+        if (from == null) {
+            return to.next;
+        } else {
+            from.next = to.next;
+            return from;
+        }
+    }
 }
