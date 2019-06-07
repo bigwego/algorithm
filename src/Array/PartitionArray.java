478e28e7fd63690a3bdcb23aca26b1617f2ddf56
zhang <zhangwei@team-lab.com>
2019-06-08 08:05:15 +0900

    update;

--- src/Array/PartitionArray.java
+++ src/Array/PartitionArray.java
@@ -1,4 +1,31 @@
 package Array;
 
 public class PartitionArray {
+
+    public int partitionArray(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            while (l <= r && nums[l] < k) {
+                l++;
+            }
+            while (l <= r && nums[r] >= k) {
+                r--;
+            }
+            if (l <= r) {
+                swap(nums, l++, r--);
+            }
+        }
+
+        return l;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
 }
