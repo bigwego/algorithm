536ec099aea173cfad03cf05543f363d12d6e3ab
zhang <zhangwei@team-lab.com>
2019-06-10 09:43:11 +0900

    update;

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,24 @@
-import BinaryTree.TreeNode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution {
 
+    public String[] findOcurrences(String text, String first, String second) {
+        String[] strs = text.split(" ");
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0, len = strs.length; i < len - 2; i++) {
+            if (strs[i].equals(first) && strs[i + 1].equals(second)) {
+                list.add(strs[i + 2]);
+            }
+        }
+
+        String[] res = new String[list.size()];
+        for (int i = 0; i < res.length; i++) {
+            res[i] = list.get(i);
+        }
+
+        return res;
+    }
 }
