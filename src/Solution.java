9ed6408f84ebeb27070796870400ee3098932432
zhang <zhangwei@team-lab.com>
2019-06-12 14:49:14 +0900

    update;

--- src/Solution.java
+++ src/Solution.java
@@ -1,23 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Solution {
 
-    public String[] findOcurrences(String text, String first, String second) {
-        String[] strs = text.split(" ");
-        List<String> list = new ArrayList<>();
+}
 
-        for (int i = 0, len = strs.length; i < len - 2; i++) {
-            if (strs[i].equals(first) && strs[i + 1].equals(second)) {
-                list.add(strs[i + 2]);
-            }
-        }
+class Super {
 
-        String[] res = new String[list.size()];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = list.get(i);
-        }
-
-        return res;
+    void method() throws RuntimeException {
     }
+}
+
+class Sub extends Super {
+
+    @Override
+    protected void method() throws Exception {
+
+    }
+}
+
+interface Inter {
+
+}
+
+abstract class A {
+
 }
