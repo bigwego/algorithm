43b2b555b15c953db3e41e17ba344c2ce815d2db
zhang <zhangwei@team-lab.com>
2019-06-13 08:14:28 +0900

    update;

--- src/String/LongestSubstringWithoutRepeatingCharacters.java
+++ src/String/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,2 +1,23 @@
+package String;
+
 public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        int[] cnts = new int[256];
+        int res = 1;
+        int i = 0;
+
+        for (int j = 0, len = s.length(); j < len; j++) {
+            int idx = s.charAt(j);
+            cnts[idx]++;
+            while (cnts[idx] == 2) {
+                cnts[s.charAt(i++)]--;
+            }
+            res = Math.max(res, j - i + 1);
+        }
+
+        return res;
+    }
 }
