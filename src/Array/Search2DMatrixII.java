b0fe18d5e793c84aab1253710884fbc052fd2cb0
zhang <zhangwei@team-lab.com>
2019-06-15 16:04:04 +0900

    update

--- src/Array/Search2DMatrixII.java
+++ src/Array/Search2DMatrixII.java
@@ -1,6 +1,6 @@
 package Array;
 
-public class Seach2DMatrixII {
+public class Search2DMatrixII {
 
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0) {
