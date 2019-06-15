9728a3876811880c14dfb7493d6f24b00778f096
zhang <zhangwei@team-lab.com>
2019-06-15 20:25:56 +0900

    update

--- src/BinaryTree/SubtreeOfAnotherTree.java
+++ src/BinaryTree/SubtreeOfAnotherTree.java
@@ -36,4 +36,17 @@ public class SubtreeOfAnotherTree {
         }
         return isSame(node1.left, node2.left) && isSame(node1.right, node2.right);
     }
+
+    public boolean isSubtree2(TreeNode s, TreeNode t) {
+        if (s == null) return false;
+        if (s.val == t.val) return help(s, t);
+        return isSubtree2(s.left, t) || isSubtree2(s.right, t);
+    }
+
+    private boolean help(TreeNode s, TreeNode t) {
+        if (s == null && t == null) return true;
+        if (s == null || t == null) return false;
+        if (s.val != t.val) return false;
+        return help(s.left, t.left) && help(s.right, t.right);
+    }
 }
