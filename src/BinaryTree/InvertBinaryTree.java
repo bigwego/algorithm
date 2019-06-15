47068ba4cae9cbecedb60c7b0d7d3d92e599828c
zhang <zhangwei@team-lab.com>
2019-06-15 20:52:05 +0900

    update

--- src/BinaryTree/InvertBinaryTree.java
+++ src/BinaryTree/InvertBinaryTree.java
@@ -21,4 +21,15 @@ public class InvertBinaryTree {
         root.right = left;
         return root;
     }
+
+    public TreeNode invertTree2(TreeNode root) {
+        if (root == null) return root;
+
+        TreeNode left = invertTree2(root.left);
+        TreeNode right = invertTree2(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+
 }
