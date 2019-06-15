ca8875a20e95c3927f90b9481c0ddb63f40de4ff
zhang <zhangwei@team-lab.com>
2019-06-15 20:09:49 +0900

    update

--- src/BinaryTree/FlattenBinaryTree2LinkedList.java
+++ src/BinaryTree/FlattenBinaryTree2LinkedList.java
@@ -1,2 +1,17 @@
+package BinaryTree;
+
+// Problem 114. Flatten Binary Tree to Linked List
 public class FlattenBinaryTree2LinkedList {
+
+    TreeNode prev;
+
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
 }
--- src/Stack/AsteroidCollision.java
+++ src/Stack/AsteroidCollision.java
@@ -1,2 +1,35 @@
+package Stack;
+
+import java.util.Stack;
+
+// Problem 735. Asteroid Collision
 public class AsteroidCollision {
+
+    public int[] asteroidCollision(int[] asteroids) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int a : asteroids) {
+            if (stack.isEmpty() || a > 0 || stack.peek() < 0) {
+                stack.push(a);
+            } else {
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() + a < 0) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty() && stack.peek() + a == 0) {
+                    stack.pop();
+                    continue;
+                }
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(a);
+                }
+            }
+        }
+
+        int[] res = new int[stack.size()];
+        for (int i = res.length - 1; i > -1; i--) {
+            res[i] = stack.pop();
+        }
+
+        return res;
+    }
 }
