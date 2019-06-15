c54c4f98ba394eeff1036cd25c28fec29b8e9a9e
zhang <zhangwei@team-lab.com>
2019-06-15 20:48:39 +0900

    update

--- src/BinaryTree/PathSum.java
+++ src/BinaryTree/PathSum.java
@@ -12,15 +12,10 @@ public class PathSum {
         return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 
-    private class TreeNode {
-        int val;
-
-        TreeNode left;
-
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
+    public boolean hasPathSum2(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) return root.val == sum;
+        return hasPathSum2(root.left, sum - root.val)
+                || hasPathSum2(root.right, sum - root.val);
     }
 }
--- src/DynamicProgramming/MaximalSquare.java
+++ src/DynamicProgramming/MaximalSquare.java
@@ -20,4 +20,28 @@ public class MaximalSquare {
         }
         return res * res;
     }
+
+    public int maximalSquare2(char[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int res = 0;
+        int[][] dp = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = matrix[i][0] - '0';
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = matrix[0][i] - '0';
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                    res = Math.max(res, dp[i][j]);
+                }
+            }
+        }
+
+        return res;
+    }
 }
