1d24ace851cb3f4f74f3d8b78fd1b0e83bfb7510
zhang <zhangwei@team-lab.com>
2019-06-16 09:22:52 +0900

    update

---
+++ src/Array/SumOfDigitsInMinimumNumber.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class SumOfDigitsInMinimumNumber {
+
+    public int sumOfDigits(int[] A) {
+        int min = A[0];
+        for (int i = 1, len = A.length; i < len; i++)
+            min = Math.min(min, A[i]);
+
+        int sum = 0;
+        while (min != 0) {
+            sum += min % 10;
+            min /= 10;
+        }
+
+        return (min & 1) == 1 ? 0 : 1;
+    }
+}
--- src/Others/BinaryGap.java
+++ src/Others/BinaryGap.java
@@ -1,4 +1,21 @@
 package Others;
 
 public class BinaryGap {
+
+    public int binaryGap(int N) {
+        int prev = -1;
+        int res = 0;
+
+        for (int i = 0; i < 32 && N != 0; i++) {
+            if ((N & 1) == 1) {
+                if (prev != -1) {
+                    res = Math.max(res, i - prev);
+                }
+                prev = i;
+            }
+            N >>>= 1;
+        }
+
+        return res;
+    }
 }
