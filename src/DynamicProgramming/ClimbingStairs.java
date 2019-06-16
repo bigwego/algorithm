d7802c671f51dd0532222fa25ad44f5caed5b3ec
zhang <zhangwei@team-lab.com>
2019-06-16 16:25:46 +0900

    update

--- src/DynamicProgramming/ClimbingStairs.java
+++ src/DynamicProgramming/ClimbingStairs.java
@@ -14,4 +14,17 @@ public class ClimbingStairs {
         }
         return minusOne;
     }
+
+    public int climbStairs2(int n) {
+        if (n < 3) return n;
+
+        int one = 2, two = 1;
+        for (int i = 2; i < n; i++) {
+            int tmp = one;
+            one += two;
+            two = tmp;
+        }
+
+        return one;
+    }
 }
