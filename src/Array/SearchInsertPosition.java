a103ac75c0fa8c5dea615e8f395ea064926b590e
zhang <zhangwei@team-lab.com>
2019-06-16 20:37:51 +0900

    update

--- src/Array/SearchInsertPosition.java
+++ src/Array/SearchInsertPosition.java
@@ -39,4 +39,19 @@ public class SearchInsertPosition {
 
         return l > r ? l : l - 1;
     }
+
+    public int searchInsert3(int[] nums, int target) {
+        if (target < nums[0]) return 0;
+        if (target > nums[nums.length - 1]) return nums.length;
+
+        int l = 0, r = nums.length - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+
+        return nums[l] < target ? l + 1 : l;
+    }
 }
