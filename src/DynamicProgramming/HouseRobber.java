8c5ea93db6fc805bb8cc9608f1716efee75f9d2a
zhang <zhangwei@team-lab.com>
2019-06-16 21:21:28 +0900

    update

--- src/DynamicProgramming/HouseRobber.java
+++ src/DynamicProgramming/HouseRobber.java
@@ -14,4 +14,18 @@ public class HouseRobber {
         }
         return Math.max(r, nr);
     }
+
+    public int rob2(int[] nums) {
+        int len = nums.length;
+        if (len == 0) return 0;
+        int r = nums[0], nr = 0;
+
+        for (int i = 1; i < len; i++) {
+            int tmp = r;
+            r = nr + nums[i];
+            nr = Math.max(nr, tmp);
+        }
+
+        return Math.max(r, nr);
+    }
 }
