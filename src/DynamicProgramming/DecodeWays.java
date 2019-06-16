a3aec153740bb4fca3518581947ebb322ca1c659
zhang <zhangwei@team-lab.com>
2019-06-16 16:15:50 +0900

    update

--- src/DynamicProgramming/DecodeWays.java
+++ src/DynamicProgramming/DecodeWays.java
@@ -21,4 +21,24 @@ public class DecodeWays {
         }
         return dp[dp.length - 1];
     }
+
+    public int numDecodings2(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+
+        for (int i = 2; i <= len; i++) {
+            int one = Integer.valueOf(s.substring(i - 1, i));
+            int two = Integer.valueOf(s.substring(i - 2, i));
+            if (0 < one && one < 10)
+                dp[i] += dp[i - 1];
+            if (9 < two && two < 27)
+                dp[i] += dp[i - 2];
+        }
+
+        return dp[len];
+    }
 }
