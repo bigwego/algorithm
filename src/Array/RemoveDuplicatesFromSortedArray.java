dde611ed5e23fb0dfc2e89b0b69983d416fea71b
zhang <zhangwei@team-lab.com>
2019-06-16 15:44:16 +0900

    update

--- src/Array/RemoveDuplicatesFromSortedArray.java
+++ src/Array/RemoveDuplicatesFromSortedArray.java
@@ -14,4 +14,16 @@ public class RemoveDuplicatesFromSortedArray {
         }
         return rec + 1;
     }
+
+    public int removeDuplicates2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int idx = 0;
+
+        for (int i = 1, len = nums.length; i < len; i++) {
+            if (nums[i] != nums[idx])
+                nums[++idx] = nums[i];
+        }
+
+        return idx + 1;
+    }
 }
