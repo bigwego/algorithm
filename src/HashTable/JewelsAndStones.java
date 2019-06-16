47914ebf23b260f843ae668cb0f00d798509ed05
zhang <zhangwei@team-lab.com>
2019-06-16 11:43:23 +0900

    update

--- src/HashTable/JewelsAndStones.java
+++ src/HashTable/JewelsAndStones.java
@@ -3,6 +3,12 @@ package HashTable;
 public class JewelsAndStones {
 
     public int numJewelsInStones(String J, String S) {
-        
+        int[] cnts = new int[256];
+        int res = 0;
+
+        for (char c : S.toCharArray()) cnts[c]++;
+        for (char c : J.toCharArray()) res += cnts[c];
+
+        return res;
     }
 }
