d695aad8f9b7bebdaead2f6ec2e51aa5d434c7fd
zhang <zhangwei@team-lab.com>
2019-06-19 07:07:51 +0900

    update

---
+++ src/UnionFind/FriendCircles.java
@@ -0,0 +1,54 @@
+package UnionFind;
+
+public class FriendCircles {
+
+    public int findCircleNum(int[][] M) {
+        int n = M.length, res = 0;
+        int[] visited = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                res++;
+                dfs(M, visited, i);
+            }
+        }
+
+        return res;
+    }
+
+    private void dfs(int[][] m, int[] visited, int i) {
+        for (int j = 0; j < m.length; j++) {
+            if (m[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(m, visited, j);
+            }
+        }
+    }
+
+    public int findCircleNum2(int[][] M) {
+        int n = M.length, cnt = n;
+        int[] u = new int[n];
+
+        for (int i = 0; i < n; i++) u[i] = i;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (M[i][j] == 1) {
+                    int ri = find(u, i);
+                    int rj = find(u, j);
+                    if (ri != rj) {
+                        u[rj] = ri;
+                        cnt--;
+                    }
+                }
+            }
+        }
+
+        return cnt;
+    }
+
+    private int find(int[] u, int i) {
+        while (i != u[i]) i = u[u[i]];
+        return i;
+    }
+}
