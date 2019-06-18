4b014f64c5fe7031cde4a0fa73419a4adfc533b6
zhang <zhangwei@team-lab.com>
2019-06-18 22:00:06 +0900

    update

---
+++ src/Others/CountPrimes.java
@@ -0,0 +1,23 @@
+package Others;
+
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+        if (n < 2) return 0;
+
+        boolean[] notPrime = new boolean[n];
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            if (!notPrime[i]) {
+                for (int j = 2; j * i < n; j++)
+                    notPrime[j * i] = true;
+            }
+        }
+
+        int res = 0;
+        for (int i = 2; i < n; i++) {
+            if(!notPrime[i]) res++;
+        }
+
+        return res;
+    }
+}
