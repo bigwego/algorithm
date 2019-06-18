5e55a1b1cc38a9df93f3f294c76ca4bd881f2063
zhang <zhangwei@team-lab.com>
2019-06-19 06:06:49 +0900

    update

--- src/HashTable/SubarraySumEqualsK.java
+++ src/HashTable/SubarraySumEqualsK.java
@@ -18,4 +18,18 @@ public class SubarraySumEqualsK {
         }
         return res;
     }
+
+    public int subarraySum2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int sum = 0, res = 0;
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) res += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return res;
+    }
 }
--- src/HashTable/SubarraySumsDivisibleByK.java
+++ src/HashTable/SubarraySumsDivisibleByK.java
@@ -1,8 +1,24 @@
 package HashTable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubarraySumsDivisibleByK {
 
     public int subarraysDivByK(int[] A, int K) {
-        return 0;
+        if (A == null || A.length == 0) return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int sum = 0, res = 0;
+
+        for (int num : A) {
+            sum = (sum + num) % K;
+            if (sum < 0) sum += K;
+            if (map.containsKey(sum)) res += map.get(sum);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return res;
     }
 }
