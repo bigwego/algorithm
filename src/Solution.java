91ca1a305f405a9c121d0f7074be18872053a9a0
zhang <zhangwei@team-lab.com>
2019-06-20 12:36:46 +0900

    update

--- src/Solution.java
+++ src/Solution.java
@@ -1,52 +1,39 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.ConcurrentModificationException;
-import java.util.Iterator;
-import java.util.List;
-
-import BinaryTree.TreeNode;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Solution {
 
-    public void removeTreeNode(TreeNode root, int value) {
-        help(root, value);
-    }
-
-    private TreeNode help(TreeNode node, int value) {
-        if (node == null) return null;
-
-        if (node.val < value) node.right = help(node.right, value);
-        else if (node.val > value) node.left = help(node.left, value);
-
-        if (node.left == null && node.right == null) return null;
-
-        TreeNode rightMin = findMin(node.right);
-        node.val = rightMin.val;
-        node.right = help(node.right, rightMin.val);
-
-        return node;
-    }
-
-    private TreeNode findMin(TreeNode node) {
-        while (node.left != null) {
-            node = node.left;
-        }
-        return node;
-    }
+    static ReentrantLock lock = new ReentrantLock();
 
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+        Condition condition = lock.newCondition();
 
-        Iterator<Integer> it = list.iterator();
-        while (it.hasNext()) {
-            Integer curr = it.next();
-            if ((curr & 1) == 0) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                System.out.println(Thread.currentThread().getName() + " : " + 1);
                 try {
-                    it.remove();
-                } catch (ConcurrentModificationException e) {
-
+                    condition.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e.getMessage(), e);
                 }
+                System.out.println(Thread.currentThread().getName() + " : " + 2);
+                lock.unlock();
             }
-        }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                lock.lock();
+                System.out.println(Thread.currentThread().getName() + " : " + 1);
+                System.out.println(Thread.currentThread().getName() + " : " + 2);
+                condition.signal();
+                lock.unlock();
+            }
+        }).start();
+
+
     }
 }
