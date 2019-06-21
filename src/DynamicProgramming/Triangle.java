888b1638ff4d4fb6dba50ea88b6b92b18797266b
zhang <zhangwei@team-lab.com>
2019-06-21 13:35:08 +0900

    update

--- src/DynamicProgramming/Triangle.java
+++ src/DynamicProgramming/Triangle.java
@@ -15,4 +15,31 @@ public class Triangle {
         }
         return dp[0];
     }
+
+    public int minimumTotal2(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        for (int i = n - 2; i > -1; i--) {
+            List<Integer> row = triangle.get(i);
+            List<Integer> next = triangle.get(i + 1);
+            for (int j = 0, len = row.size(); j < len; j++) {
+                row.set(j, Math.min(next.get(j), next.get(j + 1)) + row.get(j));
+            }
+        }
+
+        return triangle.get(0).get(0);
+    }
+
+    public int minimumTotal3(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n + 1];
+
+        for (int i = n - 1; i > -1; i--) {
+            List<Integer> row = triangle.get(i);
+            for (int j = 0, len = row.size(); j < len; j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + row.get(j);
+            }
+        }
+
+        return dp[0];
+    }
 }
