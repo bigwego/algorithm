62e965b98ba4263aa8cce231e3ca8e176cea8a1d
zhang <zhangwei@team-lab.com>
2019-06-21 13:44:37 +0900

    update

--- src/DynamicProgramming/RussianDollEnvelopes.java
+++ src/DynamicProgramming/RussianDollEnvelopes.java
@@ -1,6 +1,26 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class RussianDollEnvelopes {
 
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0) return 0;
+        Arrays.sort(envelopes, (e1, e2) -> e1[0] - e2[0]);
 
+        int n = envelopes.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int res = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
 }
