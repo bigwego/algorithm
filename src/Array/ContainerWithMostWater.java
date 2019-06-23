0df5e9e349066564b496c29776d330addff1ae32
zhang <zhangwei@team-lab.com>
2019-06-23 21:21:38 +0900

    update

--- src/Array/ContainerWithMostWater.java
+++ src/Array/ContainerWithMostWater.java
@@ -14,4 +14,18 @@ public class ContainerWithMostWater {
         }
         return res;
     }
+
+    public int maxArea2(int[] height) {
+        int len = height.length;
+        int l = 0, r = len - 1;
+        int res = 0;
+
+        while (l < r) {
+            res = Math.max(res, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) l++;
+            else r--;
+        }
+
+        return res;
+    }
 }
