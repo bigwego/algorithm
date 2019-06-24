3f385bf123de598a5e8bd267d4ec36ae3981d736
zhang <zhangwei@team-lab.com>
2019-06-24 09:43:10 +0900

    update

--- src/Array/MajorityElement.java
+++ src/Array/MajorityElement.java
@@ -15,4 +15,14 @@ public class MajorityElement {
 
         return candidate;
     }
+
+    public int majorityElement2(int[] nums) {
+        int major = 0, cnt = 0;
+        for (int num : nums) {
+            if (cnt == 0) major = num;
+            cnt = major == num ? cnt + 1 : cnt - 1;
+        }
+
+        return major;
+    }
 }
