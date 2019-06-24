e236e4f0dd595e007e5a15cdde58293dffa6d87a
zhang <zhangwei@team-lab.com>
2019-06-24 10:05:28 +0900

    update

--- src/Array/SearchInRotatedSortedArray.java
+++ src/Array/SearchInRotatedSortedArray.java
@@ -52,4 +52,21 @@ public class SearchInRotatedSortedArray {
 
         return a[l] == target ? l : -1;
     }
+
+    public int search3(int[] a, int target) {
+        if (a == null || a.length == 0) return -1;
+
+        int l = 0, r = a.length - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            int num = a[mid] >= a[0] == target >= a[0] ?
+                    a[mid] : target < a[0] ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            if (num == target) return mid;
+            else if (num > target) r = mid - 1;
+            else l = mid + 1;
+        }
+
+        return a[l] == target ? l : -1;
+    }
 }
