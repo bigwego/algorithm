2f382ccb517052144fbb5319073c788da0cd3ce8
zhang <zhangwei@team-lab.com>
2019-06-24 10:22:43 +0900

    update

--- src/BinaryTree/BinaryTreeMaximumPathSum.java
+++ src/BinaryTree/BinaryTreeMaximumPathSum.java
@@ -27,4 +27,17 @@ public class BinaryTreeMaximumPathSum {
         res = Math.max(res, Math.max(root.val + right + left, root.val + Math.max(left, right)));
         return root.val + Math.max(left, right);
     }
+
+    public int maxPathSum2(TreeNode root) {
+        help2(root);
+        return res;
+    }
+
+    private int help2(TreeNode node) {
+        if (node == null) return 0;
+        int left = Math.max(help2(node.left), 0);
+        int right = Math.max(help2(node.right), 0);
+        res = Math.max(res, node.val + left + right);
+        return Math.max(left, right) + node.val;
+    }
 }
