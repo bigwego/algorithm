6c6d96ed20e265d01b2460f5e0212761385639f6
zhang <zhangwei@team-lab.com>
2019-06-24 21:34:24 +0900

    update

--- src/Array/SlidingWindowMaximum.java
+++ src/Array/SlidingWindowMaximum.java
@@ -1,8 +1,28 @@
 package Array;
 
+import java.util.LinkedList;
+
 public class SlidingWindowMaximum {
 
-    public static void main(String[] args) {
-        System.out.println(Math.sqrt(Integer.MAX_VALUE));
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return new int[]{};
+
+        int len = nums.length;
+        int[] res = new int[len - k + 1];
+        LinkedList<Integer> queue = new LinkedList<>();
+        int idx = 0;
+
+        for (int i = 0; i < len; i++) {
+            while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i])
+                queue.pollLast();
+            queue.addLast(i);
+            if (i + 1 >= k) {
+                if (queue.peekFirst() == i - k)
+                    queue.pollFirst();
+                res[idx++] = nums[queue.peekFirst()];
+            }
+        }
+
+        return res;
     }
 }
