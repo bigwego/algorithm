0932f7f58c2476f3b45545325de4266b885c497e
zhang <zhangwei@team-lab.com>
2019-06-24 17:42:29 +0900

    update

--- src/Array/IncreasingTripletSubsequence.java
+++ src/Array/IncreasingTripletSubsequence.java
@@ -15,4 +15,19 @@ public class IncreasingTripletSubsequence {
         }
         return false;
     }
+
+    public boolean increasingTriplet2(int[] nums) {
+        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= min1) {
+                min2 = min1;
+                min1 = num;
+            } else if (num <= min2) {
+                min2 = num;
+            }else return true;
+        }
+
+        return false;
+    }
 }
