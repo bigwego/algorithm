fcae33c9cad8f6f0e38d42fa3187f6462af5cf29
zhang <zhangwei@team-lab.com>
2019-06-24 17:30:10 +0900

    update

--- src/Array/MaximumProductSubarray.java
+++ src/Array/MaximumProductSubarray.java
@@ -13,4 +13,18 @@ public class MaximumProductSubarray {
         }
         return res;
     }
+
+    public int maxProduct2(int[] nums) {
+        int max = nums[0], min = nums[0], res = nums[0];
+
+        for (int i = 1, len = nums.length; i < len; i++) {
+            int currMax = max * nums[i];
+            int currMin = min * nums[i];
+            max = Math.max(nums[i], Math.max(currMax, currMin));
+            min = Math.min(nums[i], Math.min(currMax, currMin));
+            res = Math.max(res,max);
+        }
+
+        return res;
+    }
 }
