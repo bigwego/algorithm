c3458955dda51d643235df148ebd76660c3f612d
zhang <zhangwei@team-lab.com>
2019-06-24 09:25:57 +0900

    update

--- src/Others/PalindromeNumber.java
+++ src/Others/PalindromeNumber.java
@@ -13,4 +13,17 @@ public class PalindromeNumber {
 
         return x == rev || x == rev / 10;
     }
+
+    public boolean isPalindrome2(int x) {
+        if (x < 0 || (x > 0 && x % 10 == 0)) return false;
+
+        int rev = 0;
+
+        while (x > rev) {
+            rev = 10 * rev + x % 10;
+            x /= 10;
+        }
+
+        return x == rev || x == rev / 10;
+    }
 }
