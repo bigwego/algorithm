04c7c3748a2ea37fecc8c3ab835981ebf19c698f
zhang <zhangwei@team-lab.com>
2019-06-25 11:36:17 +0900

    update

--- src/String/LongestSubstringWithAtLeastKRepeatingChars.java
+++ src/String/LongestSubstringWithAtLeastKRepeatingChars.java
@@ -26,4 +26,36 @@ public class LongestSubstringWithAtLeastKRepeatingChars {
         }
         return res;
     }
+
+    public int longestSubstring2(String s, int k) {
+        int res = 0;
+
+        for (int uniqueTarget = 1; uniqueTarget <= 26; uniqueTarget++)
+            res = Math.max(res, longestSubstringWithUniqueChars(s, k, uniqueTarget));
+
+        return res;
+    }
+
+    private int longestSubstringWithUniqueChars(String s, int k, int uniqueTarget) {
+        int[] cnts = new int[26];
+        int unique = 0, noLessThanK = 0;
+        int i = 0, j = 0, len = s.length(), idx = 0;
+        int res = 0;
+
+        while (j < len) {
+            idx = s.charAt(j) - 'a';
+            if (cnts[idx]++ == 0) unique++;
+            if (cnts[idx++] == k) noLessThanK++;
+
+            while (unique > uniqueTarget) {
+                if (cnts[s.charAt(i) - 'a']-- == k) noLessThanK--;
+                if (cnts[s.charAt(i++) - 'a'] == 0) unique--;
+            }
+
+            if (unique == uniqueTarget && unique == noLessThanK)
+                res = Math.max(res, j - i);
+        }
+
+        return res;
+    }
 }
