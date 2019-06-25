7309a1aac4cc597a289bb920877c0d37589a131e
zhang <zhangwei@team-lab.com>
2019-06-25 12:45:57 +0900

    update

--- src/DynamicProgramming/PerfectSquares.java
+++ src/DynamicProgramming/PerfectSquares.java
@@ -32,4 +32,17 @@ public class PerfectSquares {
 
         return dp[n];
     }
+
+    public int numSquares3(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, n);
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+
+        return dp[n];
+    }
 }
