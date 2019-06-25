e93b6e970c397099d2151e5a0c24f31dd1046144
zhang <zhangwei@team-lab.com>
2019-06-25 17:12:44 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,57 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.channels.SelectableChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Main {
+    static Selector selector;
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-
-        int[][] points = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            points[i][0] = scanner.nextInt();
-            points[i][1] = scanner.nextInt();
+    static {
+        try {
+            selector = Selector.open();
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
         }
-
-        solve(points, n);
     }
 
-    private static void solve(int[][] points, int n) {
-        Arrays.sort(points, (p1, p2) -> p1[0] - p2[0]);
+    public static void main(String[] args) {
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
 
-        boolean[] isValid = new boolean[n];
-        isValid[n - 1] = true;
+            serverSocketChannel.bind(new InetSocketAddress(8989));
+            serverSocketChannel.configureBlocking(false);
 
-        int prevMaxY = points[n - 1][1];
-        for (int i = n - 2; i > -1; i--) {
-            if (points[i][1] <= prevMaxY) continue;
-            isValid[i] = true;
-            prevMaxY = points[i][1];
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            while (true) {
+                selector.select();
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> it = keys.iterator();
+
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+                    it.remove();
+                    process(key);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
 
-        for (int i = 0; i < n; i++) {
-            if (isValid[i])
-                System.out.println(points[i][0] + " " + points[i][1]);
+    private static void process(SelectionKey key) throws IOException {
+        if (key.isAcceptable()) {
+            ServerSocketChannel channel = (ServerSocketChannel) key.channel();
+            SocketChannel socketChannel = channel.accept();
+            socketChannel.configureBlocking(false);
+            socketChannel.register(selector, SelectionKey.OP_CONNECT);
         }
     }
 }
