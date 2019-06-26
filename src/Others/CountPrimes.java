60d9fc947cf5065874a53b12a821bc70f9dfa659
zhang <zhangwei@team-lab.com>
2019-06-26 18:09:31 +0900

    fix

--- src/Others/CountPrimes.java
+++ src/Others/CountPrimes.java
@@ -15,7 +15,24 @@ public class CountPrimes {
 
         int res = 0;
         for (int i = 2; i < n; i++) {
-            if(!notPrime[i]) res++;
+            if (!notPrime[i]) res++;
+        }
+
+        return res;
+    }
+
+    public int countPrimes2(int n) {
+        if (n < 2) return 0;
+
+        boolean[] notPrime = new boolean[n];
+        int res = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (!notPrime[i]) {
+                res++;
+                for (int j = 2; j * i < n; j++)
+                    notPrime[j * i] = true;
+            }
         }
 
         return res;
