611799c9c668cdeb354b8494b310f92d4f88386c
zhang <zhangwei@team-lab.com>
2019-06-26 17:59:27 +0900

    fix

---
+++ src/BitManipulation/ReverseBits.java
@@ -0,0 +1,16 @@
+package BitManipulation;
+
+public class ReverseBits {
+
+    public int reverseBits(int n) {
+        int res = 0;
+
+        for (int i = 0; i < 32; i++) {
+            res += n & 1;
+            n >>>= 1;
+            if (i < 31) res <<= 1;
+        }
+
+        return res;
+    }
+}
