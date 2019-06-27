0c2e36e8480e745bebac185f6f5966c7e6525e51
zhang <zhangwei@team-lab.com>
2019-06-27 09:59:04 +0900

    fix

--- src/DynamicProgramming/MaximalSquare.java
+++ src/DynamicProgramming/MaximalSquare.java
@@ -44,4 +44,26 @@ public class MaximalSquare {
 
         return res;
     }
+
+    public int maximalSquare3(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+
+        int m = matrix.length, n = matrix[0].length;
+        int[] dp = new int[n + 1];
+        int max = 0, pre = 0;
+
+        for (int i = 1; i <= m; i++) {
+            pre = 0;
+            for (int j = 1; j <= n; j++) {
+                int tmp = dp[j];
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[j] = 1 + Math.min(pre, Math.min(dp[j], dp[j - 1]));
+                    max = Math.max(max, dp[j]);
+                } else dp[j] = 0;
+                pre = tmp;
+            }
+        }
+
+        return max * max;
+    }
 }
