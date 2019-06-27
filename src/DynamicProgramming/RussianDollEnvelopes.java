8a69d2ec2fa045628d433aad0f734aa63ecc4899
zhang <zhangwei@team-lab.com>
2019-06-27 10:50:43 +0900

    fix

--- src/DynamicProgramming/RussianDollEnvelopes.java
+++ src/DynamicProgramming/RussianDollEnvelopes.java
@@ -23,4 +23,31 @@ public class RussianDollEnvelopes {
 
         return res;
     }
+
+    public int maxEnvelopes2(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0) return 0;
+
+        Arrays.sort(envelopes, (e1, e2) -> {
+            if (e1[0] == e2[0]) return e2[1] - e1[1];
+            return e1[0] - e2[0];
+        });
+
+        int len = envelopes.length;
+        int[] tails = new int[len];
+        int size = 0;
+
+        for (int[] e : envelopes) {
+            int l = 0, r = size;
+            while (l < r) {
+                int mid = l + (r - l) / 2;
+                if (tails[mid] < e[1]) mid = l + 1;
+                else r = mid;
+            }
+            tails[l] = e[1];
+            if (l == size) size++;
+        }
+
+        return size;
+    }
+
 }
