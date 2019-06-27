b0f39f8227985c28381e90ab2888a578dee7bc88
zhang <zhangwei@team-lab.com>
2019-06-27 14:17:20 +0900

    fix

--- src/LinkedList/IntersectionOfTwoLinkedList.java
+++ src/LinkedList/IntersectionOfTwoLinkedList.java
@@ -13,4 +13,17 @@ public class IntersectionOfTwoLinkedList {
 
         return a;
     }
+
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode a = headA, b = headB;
+
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
+        }
+
+        return a;
+    }
 }
