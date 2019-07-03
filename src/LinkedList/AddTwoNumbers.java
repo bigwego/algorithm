5cb4c37cf07dba77f829cf6715cd4ab528df9219
zhang <zhangwei@team-lab.com>
2019-07-03 09:34:07 +0900

    update

--- src/LinkedList/AddTwoNumbers.java
+++ src/LinkedList/AddTwoNumbers.java
@@ -26,4 +26,38 @@ public class AddTwoNumbers {
         }
         return dummy.next;
     }
+
+    public ListNode addTwoNumbers2(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) return l1 == null ? l2 : l1;
+
+        ListNode dummy = new ListNode(0), tail = dummy;
+        int c = 0;
+
+        while (l1 != null && l2 != null) {
+            c += l1.val + l2.val;
+            tail.next = new ListNode(c % 10);
+            c /= 10;
+            tail = tail.next;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        if (l1 == null && l2 == null) {
+            if (c != 0) tail.next = new ListNode(c);
+            return dummy.next;
+        }
+
+        ListNode next = l1 != null ? l1 : l2;
+        while (c > 0 && next != null) {
+            c += l1.val;
+            l1.val = c % 10;
+            c /= 10;
+            next = next.next;
+        }
+
+        if (c != 0) next.next = new ListNode(c);
+
+        tail.next = next;
+        return dummy.next;
+    }
 }
