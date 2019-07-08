758ffff73a25ed23b5c039d5f7d0e165ff135188
zhang <zhangwei@team-lab.com>
2019-07-08 14:40:52 +0900

    update

--- src/Array/Solution.java
+++ src/Array/Solution.java
@@ -1,5 +1,11 @@
 package Array;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import BinaryTree.TreeNode;
+
 public class Solution {
 
     public void change(String str) {
@@ -33,6 +39,48 @@ public class Solution {
         }
 
         return arr[l] == target ? l : -1;
+    }
+
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] res = new int[n];
+
+        Arrays.sort(bookings, (b1, b2) -> b1[0] - b2[0]);
+
+        
+    }
+
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        LinkedList<TreeNode> res = new LinkedList<>();
+        if (root == null || to_delete == null || to_delete.length == 0) return res;
+
+        res.add(root);
+
+        for (int i = 0; i < to_delete.length && !res.isEmpty(); i++) {
+            int d = to_delete[i];
+            for (int j = 0; j < res.size(); j++) {
+                TreeNode curr = res.poll();
+                del(null, curr, res, d);
+            }
+        }
+
+        return res;
+    }
+
+    private void del(TreeNode p, TreeNode curr, LinkedList<TreeNode> res, int d) {
+        if (curr == null) return;
+
+        if (curr.val == d) {
+            if (p != null) {
+                if (p.left == curr) p.left = null;
+                else p.right = null;
+            }
+            if (curr.left != null) res.add(curr.left);
+            if (curr.right != null) res.add(curr.right);
+            return;
+        }
+
+        del(curr, curr.left, res, d);
+        del(curr, curr.right, res, d);
     }
 
     public static void main(String[] args) {
