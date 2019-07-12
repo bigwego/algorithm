665e6352f23ab89332a9da05c2baa6e2e510510d
zhang <zhangwei@team-lab.com>
2019-07-12 12:00:35 +0900

    update

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,8 @@ public class Main {
         }
     }
 
+    
+
     public static void main(String[] args) {
         try {
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
